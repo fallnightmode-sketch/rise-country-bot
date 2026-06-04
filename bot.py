63a441fd270a7e41845d92c8b7fedc54254d81c3
fallnightmode-sketch <fallnightmode@gmail.com>
2026-06-04 22:37:04 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -240,7 +240,7 @@ class LOAForm(discord.ui.Modal, title="Leave of Absence Application"):
     q2 = discord.ui.TextInput(label="2. Position / Department", placeholder="Please enter your position or department.", required=True, max_length=70)
     q3 = discord.ui.TextInput(label="3. LOA End Date Only (Format: DD/MM/YYYY)", placeholder="Please enter the end date of your Leave of Absence (LOA). *(e.g., 01/06/2026)*.", required=True, max_length=15)
     q4 = discord.ui.TextInput(label="4. Reason & Notes", placeholder="Please provide the reason and any additional notes regarding your leave request.", style=discord.TextStyle.long, required=True, max_length=400)
-    q5 = discord.ui.TextInput(label="5. Reachable during leave? (Yes / No)", placeholder="Please provide a response using **Yes** or **No** only.", required=True, max_length=300)
+    q5 = discord.ui.TextInput(label="5. Reachable during leave? (Yes / No)", placeholder="Please provide a response using Yes or No only.", required=True, max_length=300)
     
     async def on_submit(self, interaction: discord.Interaction):
         if interaction.guild_id != GUILD_ID: return
